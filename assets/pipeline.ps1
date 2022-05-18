7cc6639eb989f819fc520a52cf5ed943d2581cfe
NzWildthing <brogan131001@gmail.com>
2022-05-18 21:19:04 +1200

    COMPX341-22A-A3 Commiting from CI/CD Pipeline

---
+++ assets/pipeline.ps1
@@ -0,0 +1,22 @@
+write-host "(0) Setting up npm..."
+npm install 
+write-host "(1) Building project..."
+npm run build 
+#$? is an automatic value, which contains the execution status 
+#of the last operation. TRUE if succeeded or FALSE if failed
+if($?){
+    write-host -ForegroundColor Green "Build Succeeded!"
+    write-host "(2) Pushing changes to git..."
+    cd ..
+    git add .
+    git commit -m "COMPX341-22A-A3 Commiting from CI/CD Pipeline"
+    git push origin main
+    write-host -ForegroundColor Green "Changes pushed to git successfully!"
+
+    write-host "(3) Starting project locally..."
+    npm run start
+}
+else{
+    write-host -ForegroundColor Red "Build Failed"
+    write-host "Unable to commit changes due to compile-time errors, please review"
+}
